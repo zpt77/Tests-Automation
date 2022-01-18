e24f4ca1833e7bb21980ea3af4d78d69a5a60757
Tomek Zubrzycki <zpt77@wp.pl>
2022-01-18 21:10:28 +0100

    minor changes

--- Interia_authentication.py
+++ Interia_authentication.py
@@ -13,7 +13,6 @@ driver = webdriver.Chrome(r'C:\Users\tzubr\chromedriver')
 login_page = "https://poczta.interia.pl/logowanie/"
 driver.get(login_page)
 
-#accept cookies
 driver.find_element_by_class_name('rodo-popup-agree').click()
 
 #create account
@@ -26,10 +25,10 @@ driver.find_element_by_xpath('//*[@id="mainApp"]/div/div/div/div/div[2]/div/div/
 #REGISTRATION FORM
 
 name = driver.find_element_by_xpath('//form/div[1]/div[1]/input')
-name.send_keys("asedq")
+name.send_keys("namexdqv")
 
 surname = driver.find_element_by_xpath('//form/div[1]/div[2]/input')
-surname.send_keys("nazwiskovbn")
+surname.send_keys("surnamexcv")
 
 day_of_birth = driver.find_element_by_xpath('//form/div[1]/div[3]/div[1]/input')
 day_of_birth.send_keys("01")
@@ -47,7 +46,6 @@ driver.find_element_by_xpath('//form/div[1]/div[4]/ul/li[1]').click()
 
 mail_address = driver.find_element_by_xpath('//form/div[1]/div[5]/div[1]/input')
 mail_address.click()
-mail_address
 
 password = ",/T\w9PAy"
 
@@ -66,11 +64,11 @@ time.sleep(2)
 
 create_btn = driver.find_element_by_xpath('//*[@id="mainApp"]/div/div/div/div/div[2]/div/form/div[2]/button')
 create_btn.click()
-#registration complete
 
 #get full mail address
 
-#solving ended session popup
+#solving ended session
+
 timeout = 8
 time.sleep(5)
 try:
@@ -81,8 +79,7 @@ except NoSuchElementException:
     print("extra action wasn't needed")
 
 try:
-    email = WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section[4]/div/div/div[1]/div/div/div')))
-    email.click()
+    WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section[4]/div/div/div[1]/div/div/div'))).click()
     mail_address = driver.find_element_by_xpath('//*[@id="wrapper"]/section[4]/div/div[1]/div[2]/div/div[4]').text
 except TimeoutException:
     print("Timeout") 
@@ -94,9 +91,7 @@ driver.close()
 driver = webdriver.Chrome(r'C:\Users\tzubr\chromedriver')
 driver.get(login_page)
 
-#accept cookies
 driver.find_element_by_class_name('rodo-popup-agree').click()
-
 login_mail = driver.find_element_by_id('email')
 login_mail.send_keys(mail_address)
 
@@ -104,6 +99,7 @@ login_password = driver.find_element_by_id('password')
 login_password.send_keys("wrongpass")
 
 driver.find_element_by_xpath('//*[@id="sitebar"]/form/button').click()
+
 time.sleep(2)
 driver.close()
 
@@ -111,7 +107,6 @@ driver.close()
 driver = webdriver.Chrome(r'C:\Users\tzubr\chromedriver')
 driver.get(login_page)
 
-#accept cookies
 driver.find_element_by_class_name('rodo-popup-agree').click()
 
 login_mail = driver.find_element_by_id('email')
@@ -121,7 +116,7 @@ login_password = driver.find_element_by_id('password')
 login_password.send_keys(password)
 
 driver.find_element_by_xpath('//*[@id="sitebar"]/form/button').click()
-#user logged
+
 #get first mail title
 
 timeout = 3
@@ -130,21 +125,3 @@ try:
     print(mailbox.text)
 except TimeoutException:
     print("Timeout") 
-
-
-
-#accept cookies
-
-
-
-
-#timeout = 3
-#try:
- #   mailbox = WebDriverWait(driver,timeout).until(EC.presence_of_element_located((By.CLASS_NAME,'msglist-item__message__subject-text')))
-  #  print(mailbox.text)
-#except TimeoutException:
- #   print("Timeout") 
-#print(driver.find_element_by_xpath('//*[@id="mainApp"]/div/div/div/div/div[2]/div/form/div[2]/button').text)
-
-
-#'//*[@id="m1"]/div[2]/div[1]/div[2]/span[2]'
